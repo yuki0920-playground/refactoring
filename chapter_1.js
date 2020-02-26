841cf3991386f55df1e65a6610b7b833155434e4
yuki0920 <ywsep20@gmail.com>
2020-02-26 12:00:45 +0900

    関数宣言の変更でplay変数を削除した

--- chapter_1.js
+++ chapter_1.js
@@ -35,7 +35,7 @@ function statement(invoice, plays) {
     }).format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -50,9 +50,9 @@ function statement(invoice, plays) {
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -67,7 +67,7 @@ function statement(invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`unknown type: ${play.type}`);
+        throw new Error(`unknown type: ${playFor(aPerformance).type.type}`);
     }
     return result
   }

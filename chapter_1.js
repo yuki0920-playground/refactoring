f9666ecfb5fcd30160c6d2767518bdf8e6fc105c
yuki0920 <ywsep20@gmail.com>
2020-02-26 11:45:57 +0900

    問い合わせによる一時変数の置き換えをした

--- chapter_1.js
+++ chapter_1.js
@@ -35,7 +35,7 @@ function statement(invoice, plays) {
     }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
 
     // add volume credits
@@ -71,6 +71,10 @@ function statement(invoice, plays) {
         throw new Error(`unknown type: ${play.type}`);
     }
     return result
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
 }
 

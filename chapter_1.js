cec3b7807e647c3a98d1ab7de3074a9ac6e6f7db
yuki0920 <ywsep20@gmail.com>
2020-02-26 21:53:21 +0900

    関数を並び替えた

--- chapter_1.js
+++ chapter_1.js
@@ -91,41 +91,6 @@ function statement(invoice, plays) {
     }
     return result
   }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
-    return result
-  }
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US",
-      {
-        style: "currency", currency: "USD",
-        minimumFractionDigits: 2
-      }).format(aNumber / 100);
-  }
-
-  function totalVolumeCredits() {
-    let volumeCredits = 0;
-    for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
-    }
-    return volumeCredits;
-  }
-
-  function totalAmount() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-      result += amountFor(perf);
-    }
-    return result;
-  }
 }
 
 console.log(statement(invoice[0], plays))

9a69f821845689c9b27122c213e19c246ef3c3b4
yuki0920 <ywsep20@gmail.com>
2020-03-01 07:47:22 +0900

    statement関数をrenderPlainTextにリネームして関数の抽出をした

--- chapter_1.js
+++ chapter_1.js
@@ -25,6 +25,11 @@ var invoice = [
 ]
 
 function statement(invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
+
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {

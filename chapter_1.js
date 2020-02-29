bc46cc9c8212bc6ab3b2ac877c996c1a55f3632e
yuki0920 <ywsep20@gmail.com>
2020-03-01 08:00:31 +0900

    performancesを中間データに追加し不要になったrenderPlainTextのinvoice引数を削除した

--- chapter_1.js
+++ chapter_1.js
@@ -27,14 +27,15 @@ var invoice = [
 function statement(invoice, plays) {
   const statementData = {};
   statementData.Customer = invoice.customer;
+  statementData.performances = invoice.performances;
   return renderPlainText(statementData, invoice, plays);
 }
 
-function renderPlainText(data, invoice, plays) {
+function renderPlainText(data, plays) {
 
   let result = `Statement for ${data.customer}\n`;
 
-  for (let perf of invoice.performances) {
+  for (let perf of data.performances) {
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   }
@@ -44,7 +45,7 @@ function renderPlainText(data, invoice, plays) {
 
   function totalAmount() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf);
     }
     return result;
@@ -52,7 +53,7 @@ function renderPlainText(data, invoice, plays) {
 
   function totalVolumeCredits() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
     return result;

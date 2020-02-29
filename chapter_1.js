182de3227df3985d96466a848d1e06e89294ea43
yuki0920 <ywsep20@gmail.com>
2020-03-01 07:50:19 +0900

    中間データ構造を追加した

--- chapter_1.js
+++ chapter_1.js
@@ -25,10 +25,11 @@ var invoice = [
 ]
 
 function statement(invoice, plays) {
-  return renderPlainText(invoice, plays);
+  const statementData = {};
+  return renderPlainText(statementData, invoice, plays);
 }
 
-function renderPlainText(invoice, plays) {
+function renderPlainText(data, invoice, plays) {
 
   let result = `Statement for ${invoice.customer}\n`;
 

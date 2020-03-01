56e69a1eaac988ae805537cc530551ea8290e1cc
yuki0920 <ywsep20@gmail.com>
2020-03-01 18:27:54 +0900

    htmlStatement関数とrenderHtml関数を追加した

--- chapter_1.js
+++ chapter_1.js
@@ -28,6 +28,10 @@ function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
 }
 
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
 function createStatementData(invoice, plays) {
   const statementData = {};
   statementData.Customer = invoice.customer;
@@ -104,6 +108,20 @@ function renderPlainText(data) {
   return result;
 }
 
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+  for (let perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+  return result;
+}
+
 function usd(aNumber) {
   return new Intl.NumberFormat("en-US",
     {
@@ -113,3 +131,4 @@ function usd(aNumber) {
 }
 
 console.log(statement(invoice[0], plays))
+console.log(htmlStatement(invoice[0], plays))

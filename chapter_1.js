dfc3ac4c13db257fa604f3966485ca9c0130630d
yuki0920 <ywsep20@gmail.com>
2020-03-01 09:48:55 +0900

    usd関数をトップレベルに移動した

--- chapter_1.js
+++ chapter_1.js
@@ -102,15 +102,14 @@ function renderPlainText(data) {
   result += `Amount owed is ${usd(data.totalAmount)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
+}
 
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US",
-      {
-        style: "currency", currency: "USD",
-        minimumFractionDigits: 2
-      }).format(aNumber / 100);
-  }
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US",
+    {
+      style: "currency", currency: "USD",
+      minimumFractionDigits: 2
+    }).format(aNumber / 100);
 }
 
 console.log(statement(invoice[0], plays))

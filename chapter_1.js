7b084c77298f88f8bc85390eae62cf1eee3b7464
yuki0920 <ywsep20@gmail.com>
2020-03-01 09:22:57 +0900

    volumeCreditsFor関数を中間データに移動し呼び出し側をdata経由に変更した

--- chapter_1.js
+++ chapter_1.js
@@ -34,6 +34,7 @@ function statement(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
 
     function playFor(aPerformance) {
@@ -59,6 +60,13 @@ function statement(invoice, plays) {
         default:
           throw new Error(`unknown type: ${aPerformance.play.type.type}`);
       }
+      return result
+    }
+
+    function volumeCreditsFor(aPerformance) {
+      let result = 0;
+      result += Math.max(aPerformance.audience - 30, 0);
+      if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
       return result
     }
   }
@@ -87,7 +95,7 @@ function renderPlainText(data) {
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
     return result;
   }
@@ -98,13 +106,6 @@ function renderPlainText(data) {
         style: "currency", currency: "USD",
         minimumFractionDigits: 2
       }).format(aNumber / 100);
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-    return result
   }
 }
 

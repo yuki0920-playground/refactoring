23f8191812394e546b014e391a64db53e444af8a
yuki0920 <ywsep20@gmail.com>
2020-03-02 22:36:28 +0900

    演劇の種類ごとの計算ロジックをサブクラスに移動した

--- statement.js
+++ statement.js
@@ -103,8 +103,14 @@ function totalVolumeCredits(data) {
 }
 
 function createPerfomanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, playFor(aPerformance));
+  switch (aPlay.type) {
+    case 'tragedy': return new TragedyCalculator(aPerformance, aPlay);
+    case 'comedy': return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error('未知の演劇の種類: ${aPlay.type}');
+  }
 }
+
 
 class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
@@ -113,25 +119,7 @@ class PerformanceCalculator {
   }
 
   get amount() {
-    let result = 0;
-    switch (this.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${this.play.type.type}`);
-    }
-    return result
+    throw new Error('サブクラスの責務');
   }
 
   get volumeCredits() {
@@ -139,6 +127,26 @@ class PerformanceCalculator {
     result += Math.max(this.performance.audience - 30, 0);
     if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
     return result
+  }
+}
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
   }
 }
 

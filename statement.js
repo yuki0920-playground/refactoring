9ad9c7a30954c830700788341c0a1d56a716cc9b
yuki0920 <ywsep20@gmail.com>
2020-03-02 21:55:09 +0900

    createStatementData関数内に入れ子になった関数をトップレベルに移動した

--- statement.js
+++ statement.js
@@ -74,32 +74,32 @@ function createStatementData(invoice, plays) {
   result.totalAmount = totalAmount(result);
   result.totalVolumeCredits = totalVolumeCredits(result);
   return result;
+}
 
-  function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
-    const result = Object.assign({}, aPerformance);
-    result.play = calculator.play;
-    result.amount = calculator.amount;
-    result.volumeCredits = calculator.volumeCredits;
-    return result;
-  }
+function enrichPerformance(aPerformance) {
+  const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+  const result = Object.assign({}, aPerformance);
+  result.play = calculator.play;
+  result.amount = calculator.amount;
+  result.volumeCredits = calculator.volumeCredits;
+  return result;
+}
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
+function playFor(aPerformance) {
+  return plays[aPerformance.playID];
+}
 
-  function totalAmount(data) {
-    return data.performances
-      .reduce((total, p) => total + p.amount, 0);
-  }
+function totalAmount(data) {
+  return data.performances
+    .reduce((total, p) => total + p.amount, 0);
+}
 
-  function totalVolumeCredits(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
+function totalVolumeCredits(data) {
+  let result = 0;
+  for (let perf of data.performances) {
+    result += perf.volumeCredits;
   }
+  return result;
 }
 
 class PerformanceCalculator {

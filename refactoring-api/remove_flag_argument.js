827b9a4de35a7b9ae3483ac8a72271e30aab6de9
yuki0920 <ywsep20@gmail.com>
2020-10-15 08:38:13 +0900

    Remove flag argument

--- refactoring-api/remove_flag_argument.js
+++ refactoring-api/remove_flag_argument.js
@@ -1,16 +1,19 @@
-function deliveryDate(anOrder, isRush) {
-  if (isRush) {
-    let deliveryTime;
-    if (['MA', 'CI'].includes(anOrder.deliveryState)) deliveryTime = 1;
-    else if (['NY', 'NH'].includes(anOrder.deliveryState)) deliveryTime = 2;
-    else deliveryTime = 3;
-    return anOrder.placedOn.plusDays(1 + deliveryTime);
-  }
-  else {
-    let deliveryTime;
-    if (['MA', 'CA', 'NY'].includes(anOrder.deliveryState)) deliveryTime = 2;
-    else if (['ME', 'NH'].includes(anOrder.deliveryState)) deliveryTime = 3;
-    else deliveryTime = 4;
-    return anOrder.placedOn.plusDays(2 + deliveryTime);
-  }
+function deliveryDate(anOrder) {
+  let deliveryTime;
+  if (['MA', 'CA', 'NY'].includes(anOrder.deliveryState)) deliveryTime = 2;
+  else if (['ME', 'NH'].includes(anOrder.deliveryState)) deliveryTime = 3;
+  else deliveryTime = 4;
+  return anOrder.placedOn.plusDays(2 + deliveryTime);
 }
+
+function rushDeliveryDate(anOrder) {
+  let deliveryTime;
+  if (['MA', 'CI'].includes(anOrder.deliveryState)) deliveryTime = 1;
+  else if (['NY', 'NH'].includes(anOrder.deliveryState)) deliveryTime = 2;
+  else deliveryTime = 3;
+  return anOrder.placedOn.plusDays(1 + deliveryTime);
+}
+
+// # ClientCode
+// aShipment.deliveryDate = rushDeliveryDate(anOrder);
+// aShipment.deliveryDate = deliveryDate(anOrder);

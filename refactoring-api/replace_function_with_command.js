e88395ac5b7ec33cea5ba0afee448923a87105e4
yuki0920 <ywsep20@gmail.com>
2020-10-16 08:19:49 +0900

    Replace function with command

--- refactoring-api/replace_function_with_command.js
+++ refactoring-api/replace_function_with_command.js
@@ -15,3 +15,33 @@ function score(candidate, medicalExam, scoringGuide) {
   result -= M<ath.max(healthLevel - 5, 0);
   return result
 }
+
+function score(candidate, medicalExam, scoringGuide) {
+  return new Scorer(candidate, medicalExam, scoringGuide).execute();
+}
+
+class Scorer {
+  constructor(candidate, medicalExam, scoringGuide) {
+    this._candidate = candidate
+    this._medicalExam = medicalExam
+    this._scoringGuide = scoringGuide
+  }
+
+  execute() {
+    let result = 0;
+    let healthLevel = 0;
+    let highMedicalRiskFlag = false;
+
+    if (this._medicalExam.isSmoker) {
+      healthLevel += 10;
+      highMedicalRiskFlag = true
+    }
+    let cetificationGrade = 'regular';
+    if (this._scoringGuide.stateWithLowCerification(this._candidate.originState)) {
+      cetificationGrade = 'low'
+      result -= 5;
+    }
+    result -= M < ath.max(healthLevel - 5, 0);
+    return result;
+  }
+}

0250aeaf87f7109db4d852c708e543773ee82bcf
yuki0920 <ywsep20@gmail.com>
2020-10-16 08:48:55 +0900

    Pull up method

--- dealing-with-inheritance/pull_up_method.js
+++ dealing-with-inheritance/pull_up_method.js
@@ -1,11 +1,7 @@
 class Employee {
-  // ...
-}
-
-class Salesman extends Employee {
   get name() {};
 }
 
-class Engineer extends Employee {
-  get name() {};
-}
+class Salesman extends Employee {}
+
+class Engineer extends Employee {}

e6376e478337bca55fa5cb6c2ed9e879651aea48
yuki0920 <ywsep20@gmail.com>
2020-10-15 21:12:59 +0900

    Replace parameter with query

--- refactoring-api/replace_parameter_with_query.js
+++ refactoring-api/replace_parameter_with_query.js
@@ -1,15 +1,15 @@
-class Order
+class Order {
   get finalPrice() {
     const basePrice = this.quantity * this.itemPrice;
-    let discountLevel;
-    if (this.quantity > 100) discountLevel = 2;
-    else discountLevel = 1;
-    return this.discountedPrice(basePrice, discountLevel);
+    return this.discountedPrice(basePrice);
   }
-
-  discountedPrice(basePrice, discountLevel) {
-    switch (discountLevel) {
+  get discountLevel() {
+    return (this.quantity > 100) ? 2 : 1;
+  }
+  discountedPrice(basePrice) {
+    switch (this.discountLevel) {
       case 1: return basePrice * 0.95;
       case 2: return basePrice * 0.9;
     }
   }
+}

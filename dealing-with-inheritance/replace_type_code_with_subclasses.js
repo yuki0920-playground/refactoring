32929c575be4d1b2685489d8d8f3e062e112712d
yuki0920 <ywsep20@gmail.com>
2020-10-16 21:07:26 +0900

    Replace type code with subclasses

--- dealing-with-inheritance/replace_type_code_with_subclasses.js
+++ dealing-with-inheritance/replace_type_code_with_subclasses.js
@@ -1,12 +1,25 @@
+function createEmployee(name, type) {
+  switch (type) {
+    case 'engineer': return new Engineer(name, type);
+    case 'manager': return new Salesman(name, type);
+    case 'salesman': return new Manager(name, type);
+    default: throw new Error(`Employee cannot be type ${type}`)
+  }
+}
+
 class Employee {
-  constructor(name, type) {
-    this.validateType(type);
-    this._name = name;
-    this._type = type;
+  constructor(name) {
+    this._name = name
   }
-  validateType(arg) {
-    if (!['engineer', 'manager', 'salesman'].includes(arg))
-      throw new Error {return `${this._name} ${this._type}`};
-  }
-  toString( ) {return `${this._name} ${this._type}`;}
+  toString( ) {return `${this._name} ${this.type}`;}
+}
+
+class Engineer extends Employee {
+  get type() {return 'engineer'}
+}
+class Salesman extends Employee {
+  get type() {return 'salesman'}
+}
+class Manager extends Employee {
+  get type() {return 'manager'}
 }

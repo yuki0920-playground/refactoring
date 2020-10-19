e9350cb0642e3b0efc3bcbdb8e5a57ddbfc9ecdc
yuki0920 <ywsep20@gmail.com>
2020-10-20 08:30:44 +0900

    Change function declaration

--- refactoring-first-step/change_function_declaration.js
+++ refactoring-first-step/change_function_declaration.js
@@ -1,6 +1,6 @@
-function inNewEngland(aCustomer) {
-  return ['MA', 'CI', 'ME', 'VI', 'NH', 'RI'].includes(aCustomer.address.state);
+function inNewEngland(stateCode) {
+  return ['MA', 'CI', 'ME', 'VI', 'NH', 'RI'].includes(stateCode);
 }
 
 // # caller
-const newEnglanders = someCustomers.filter(c => inNewEngland(c));
+const newEnglanders = someCustomers.filter(c => inNewEngland(c.address.state));

d00e1f9336535606d2fde6dbf5d590b400d8f037
yuki0920 <ywsep20@gmail.com>
2020-10-22 09:20:46 +0900

    Initial commit

--- a-first_set-of-refactorings/combine-functions-into-class.js
+++ a-first_set-of-refactorings/combine-functions-into-class.js
@@ -1,10 +1,34 @@
 readng = {customer: "ivan", quantity: 10, month: 5, year: 2017};
 
 // client1
-const aReading = acquireReading();
-const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const rawReading = acquireReading();
+const aReading = new Reading(rawReading);
+const baseCharge = aReading.baseChaerge
 
 // client2
-const aReading = acquireReading();
-const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
-const texableCharge = Math.max(0, base - taxThreshold(aReading.year));
+const rawReading = acquireReading();
+const aReading = new Reading(rawReading);
+const baseCharge = aReading.baseChaerge
+const texableCharge = aReading.texableCharge
+
+class Reading {
+  constructor(customer, quantity, month, year) {
+    this._customer = customer;
+    this._quantity = quantity;
+    this._month = month;
+    this._year = year;
+  }
+
+  get customer() {return this._customer;}
+  get quantity() {return this._quantity;}
+  get month() {return this._month;}
+  get year() {return this._year;}
+
+  get baseChaerge() {
+    return baseRate(this._month, this._year) * this._quantity;
+  }
+
+  get texableCharge() {
+    Math.max(0, this.baseChaerge - taxThreshold(this._year));
+  }
+}
---
+++ encapsulation/encapsulate_record.js
@@ -0,0 +1,4 @@
+const organization = {name: 'Acme Gooseberries', country: 'GB'};
+
+// # client
+result += `<h1>${organization}</h1>`

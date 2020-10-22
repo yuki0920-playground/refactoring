aa076eaff3e0b2d708f49795a4cca6589d4bc9fb
yuki0920 <ywsep20@gmail.com>
2020-10-22 09:24:53 +0900

    Encapsulate record

--- encapsulation/encapsulate_record.js
+++ encapsulation/encapsulate_record.js
@@ -1,4 +1,12 @@
-const organization = {name: 'Acme Gooseberries', country: 'GB'};
+const organization = new Organization('Acme Gooseberries', 'GB')
 
 // # client
-result += `<h1>${organization}</h1>`
+result += `<h1>${organization.name}</h1>`
+
+class Organization{
+  constructor(data) {
+    this.name = data.name;
+    this.country = data.country;
+  }
+  get name() {return this.name};
+}

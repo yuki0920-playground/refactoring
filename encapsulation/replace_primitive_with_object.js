0c7c31ba3eb7bb1fa5a8475e831f99fd5effdd6c
yuki0920 <ywsep20@gmail.com>
2020-10-23 09:04:51 +0900

    Move method

--- encapsulation/replace_primitive_with_object.js
+++ encapsulation/replace_primitive_with_object.js
@@ -9,9 +9,21 @@ class Order {
 class Priority {
   constructor(value) {this.value = value;}
   toString() {return this.value;}
+  get index() {
+    return Priority.legalValues().findIndex(legalValue =>
+      legalValue === this.value
+    );
+  }
+  static legalValues() {return ['low', 'normal', 'high', 'rush'];}
+  higherThan(otherPriority) {
+    this.index > otherPriority.index;
+  }
+  lowerThan(otherPriority) {
+    this.index < otherPriority.index;
+  }
 }
 
 // # client
 highPriorityCount = orders.filter(order =>
-  order.priorityString === 'high' || order.priorityString === 'rush'
+  order.priorityString.higherThan(new Priority('normal'))
 ).length
